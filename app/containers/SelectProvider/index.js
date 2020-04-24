826292f32120db95c39301912226a7a25c3f4aff
Tobias Winkler <tobias.winkler@axelspringer.de>
2020-04-25 00:16:13 +0200

    moved form fields into separate components
    Signed-off-by: Tobias Winkler <tobias.winkler@axelspringer.de>

---
+++ app/containers/SelectProvider/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Input, Form } from 'semantic-ui-react';
+
+export default () => (
+  <Form.Field required>
+    <Input placeholder="Provider" fluid />
+  </Form.Field>
+);
--- app/containers/SetReminderPage/index.js
+++ app/containers/SetReminderPage/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Header, Container, Input, Button, Form } from 'semantic-ui-react';
+
+import SelectCategory from 'containers/SelectCategory';
+import SelectProviders from 'containers/SelectProvider';
+
 import messages from './messages';
 
 export default function SetReminderPage() {
@@ -13,19 +17,15 @@ export default function SetReminderPage() {
         <Form.Field required>
           <Input placeholder="Title" fluid />
         </Form.Field>
-        <Form.Field required>
-          <Input placeholder="Category" fluid />
-        </Form.Field>
-        <Form.Field required>
-          <Input placeholder="Provider" fluid />
-        </Form.Field>
+        <SelectCategory />
+        <SelectProviders />
         <Form.Field required>
           <Input placeholder="Contract End Date" fluid />
         </Form.Field>
         <Form.Field>
           <Input placeholder="Notice Period" fluid />
         </Form.Field>
-        <Button type="submit">next</Button>
+        <Button type="submit" content="next" />
       </Form>
     </Container>
   );
