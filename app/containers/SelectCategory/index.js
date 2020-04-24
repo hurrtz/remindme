68358705387ffd016c0cfdf049b1ffec5ad86ac3
Tobias Winkler <tobias.winkler@axelspringer.de>
2020-04-25 01:16:18 +0200

    wrapped form fields for category and provider with redux
    Signed-off-by: Tobias Winkler <tobias.winkler@axelspringer.de>

--- app/containers/SelectCategory/index.js
+++ app/containers/SelectCategory/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import { Input, Form } from 'semantic-ui-react';
+import React, { memo } from 'react';
+import { Form } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+import PropTypes from 'prop-types';
 
-export default () => (
+import { makeSelectCategory } from 'containers/SetReminderPage/selectors';
+import { setCategory } from 'containers/SetReminderPage/actions';
+
+const SelectCategory = ({ category, onChange }) => (
   <Form.Field required>
-    <Input placeholder="Category" fluid />
+    <Form.Input
+      placeholder="Category"
+      fluid
+      value={category}
+      onChange={onChange}
+    />
   </Form.Field>
 );
+
+SelectCategory.propTypes = {
+  category: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  category: makeSelectCategory(),
+});
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onChange: category => dispatch(setCategory(category.target.value)),
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(SelectCategory);
--- app/containers/SelectProvider/index.js
+++ app/containers/SelectProvider/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import { Input, Form } from 'semantic-ui-react';
+import React, { memo } from 'react';
+import { Form } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+import PropTypes from 'prop-types';
 
-export default () => (
+import { makeSelectProvider } from 'containers/SetReminderPage/selectors';
+import { setProvider } from 'containers/SetReminderPage/actions';
+
+const SelectCategory = ({ provider, onChange }) => (
   <Form.Field required>
-    <Input placeholder="Provider" fluid />
+    <Form.Input
+      placeholder="Provider"
+      fluid
+      value={provider}
+      onChange={onChange}
+    />
   </Form.Field>
 );
+
+SelectCategory.propTypes = {
+  provider: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  provider: makeSelectProvider(),
+});
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onChange: provider => dispatch(setProvider(provider.target.value)),
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(SelectCategory);
